ce6cd0bd0aa6d8f0aa0613cb6bc2fdc8d0835b8f
Juan Lago <juanlagodelrio@gmail.com>
2016-05-27 17:01:52 +0200

    first commit

---
+++ src/net/iessanclemente/a13juanlr/katafizzbuzz/katafizzbuzz.java
@@ -0,0 +1,10 @@
+package net.iessanclemente.a13juanlr.katafizzbuzz;
+
+public class katafizzbuzz {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ tests/net/iessanclemente/a13juanlr/katafizzbuzz/tests/katafizzbuzztests.java
@@ -0,0 +1,14 @@
+package net.iessanclemente.a13juanlr.katafizzbuzz.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class katafizzbuzztests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
